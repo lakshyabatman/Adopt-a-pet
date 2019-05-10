2b3a2747709cc7783db7e3930d29c0a5e7ced41d
lakshyabatman <lakshya.khera@gmail.com>
2019-05-11 03:01:11 +0530

    Project Done

--- src/store/mutation.js
+++ src/store/mutation.js
@@ -1,3 +1,6 @@
-export default{
-    
+
+export default {
+  appendPet:( state, { pet, species } )=>{
+    state[species].push(pet)
+  } 
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,95 @@
 <template>
   <div class="home">
-      Adopt A New Best Friend
+       <h1>Adopt A New Best Friend</h1>
+       <button class="btn btn-primary" @click="togglePetForm">Add New Pet</button>
+       
+        <div>
+          <b-form @submit.prevent="submitForm"  v-if="petForm">
+            
+
+            <b-form-group id="input-group-2" label="Pet's Name:" label-for="input-2">
+              <b-form-input
+                id="input-2"
+                v-model="form.first_name"
+                required
+                placeholder="Enter name"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-3" label="Spcies:" label-for="input-3">
+              <b-form-select
+                id="input-3"
+                v-model="form.species"
+                :options="['cats','dogs']"
+                required
+              ></b-form-select>
+            </b-form-group> 
+              <b-form-group id="input-group-2" label="Pet's Age:" label-for="input-2">
+              <b-form-input
+                id="input-2" 
+                type="number" 
+                v-model="form.age"
+                required
+                placeholder="Enter Age"
+              ></b-form-input>
+              </b-form-group>
+              <b-form-group id="input-group-2" label="Pet's Breed:" label-for="input-2">
+              <b-form-input
+                id="input-2" 
+                type="text" 
+                v-model="form.breed"
+                required
+                placeholder="Enter Breed"
+              ></b-form-input>
+            </b-form-group>
+            
+
+            <b-button type="submit" variant="primary">Submit</b-button>
+          </b-form>
+          <b-card class="mt-3" header="Form Data Result">
+            <pre class="m-0">{{ form }}</pre>
+          </b-card>
+        </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import {mapActions} from 'vuex'
 export default {
-  name: 'home'
+  name: 'home',
+  data() {
+    return {
+    petForm:false,
+    form:{
+      first_name:"" ,
+      age: "" ,
+      species: null
+    }
+
+    }
+  },
+  methods: {
+    ...mapActions([
+      'addPet'
+    ]),
+    togglePetForm() {
+      this.petForm= !this.petForm
+    },
+    submitForm(){
+      const { species, age, first_name,breed }=this.form 
+      const payload={
+        species,
+        pet:{
+          first_name ,
+          age ,
+          breed,
+        }
+      }
+      this.addPet(payload)
+      this.form={
+        
+      }
+    }
+  }
 }
 </script>
